38c76beeffc95b71e2fca8f18f82d439931bc581
Abdul Manan <abdul.manan@novatoresols.com>
2026-02-27 09:38:53 +0500

    latest code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const cookiesParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-
 
 const authRoute = require("./routes/auth.route");
 
 app.use(express.json());
+app.use(cookieParser()); 
 
-app.use("/api/auth", authRoute);
-app.use(cookiesParser());
+app.use("/api/auth", authRoute);    
 
 module.exports = app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,34 +1,58 @@
 const usermodel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 
-function userRegisterCOntroller(req, res) {
+async function userRegisterCOntroller(req, res) {
+  try {
+    const { email, name, password } = req.body;
 
-    const {email, name, password} = req.body;
-            const isExisting = await usermodel.findOne({ email });
+    const isExisting = await usermodel.findOne({ email });
 
     if (isExisting) {
-        return res.status(422).json({message: "Email already exists", status: "failed"});
+      return res.status(422).json({
+        message: "Email already exists",
+        status: "failed",
+      });
     }
 
     const user = await usermodel.create({
-        email,
-        name,
-        password
-    }); 
+      email,
+      name,
+      password,
+    });
 
-    const  token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "3d"});
-    res.cookie("token", token); 
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.cookie("token", token);
 
     res.status(201).json({
-        user:{
+      user: {
         _id: user._id,
         email: user.email,
-        name: user.name 
-    },
-    token 
-
-
-});
+        name: user.name,
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
 }
 
-module.exports = {userRegisterCOntroller};
+async function userLoginController(req, res) {
+    const { email, password } = req.body;
+
+    const user = await usermodel.findOne({ email })
+
+    if (!user) {
+      return res.status(401).json({
+        message: "email or password is incorrect",
+        status: "failed",
+      });
+    }
+}
+
+module.exports = { userRegisterCOntroller, userLoginController };
