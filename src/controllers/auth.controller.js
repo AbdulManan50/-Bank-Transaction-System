05b0ce358223f84c5bf2ccae31fbc85ecc0469d3
Abdul Manan <abdul.manan@novatoresols.com>
2026-03-03 16:42:18 +0500

    my code

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const usermodel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const { sendRegistrationEmail } = require("../services/email.service");
 
 async function userRegisterCOntroller(req, res) {
   try {
@@ -34,6 +35,7 @@ async function userRegisterCOntroller(req, res) {
       },
       token,
     });
+    await sendRegistrationEmail(user.email, user.name);
   } catch (error) {
     res.status(500).json({
       message: "Server Error",
@@ -43,16 +45,40 @@ async function userRegisterCOntroller(req, res) {
 }
 
 async function userLoginController(req, res) {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await usermodel.findOne({ email })
+  const user = await usermodel.findOne({ email });
 
-    if (!user) {
-      return res.status(401).json({
-        message: "email or password is incorrect",
-        status: "failed",
-      });
-    }
+  if (!user) {
+    return res.status(401).json({
+      message: "email or password is incorrect",
+      status: "failed",
+    });
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) {
+    return res.status(401).json({
+      message: "email or password is incorrect",
+      status: "failed",
+    });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
 }
 
 module.exports = { userRegisterCOntroller, userLoginController };
