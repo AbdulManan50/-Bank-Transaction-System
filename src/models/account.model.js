996bc45564b01f6b2fd7fcd5eb574014f0b3951a
Abdul Manan <abdul.manan@novatoresols.com>
2026-03-04 15:49:19 +0500

    account

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -4,24 +4,27 @@ const accountSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "account must be associated with a user"],
+      ref: "user",
+      required: [true, "Account must be associated with a user"],
       index: true,
     },
     status: {
+      type: String,
       enum: {
-        values: ["active", "closed", "frozen"],
-        message: "Status must be either active, closed, or frozen",
-        default: "active",
+        values: ["ACTIVE", "FROZEN", "CLOSED"],
+        message: "Status can be either ACTIVE, FROZEN or CLOSED",
       },
+      default: "ACTIVE",
     },
     currency: {
       type: String,
       required: [true, "Currency is required for creating an account"],
-      default: "PKR",
+      default: "INR",
     },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
 
 accountSchema.index({ user: 1, status: 1 });
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -2,13 +2,12 @@ const authMiddleware = require("../middleware/auth.middleware");
 const accountController = require("../controllers/account.controller");
 const express = require("express");
 
-
-
-
 const router = express.Router();
 
-
-router.post("/", authMiddleware.Middleware, accountController.createAccountController);
-
+router.post(
+  "/",
+  authMiddleware.Middleware,
+  accountController.createAccountController,
+);
 
 module.exports = router;
